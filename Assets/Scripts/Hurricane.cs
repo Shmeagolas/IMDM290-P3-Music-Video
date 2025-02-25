ec9fd6d5544333f78a0daa78497fb7761a0f4651
Ethan Paley <epaley360@icloud.com>
2025-02-24 21:23:28 -0500

    Ethan's Storm

--- Assets/Scripts/Hurricane.cs
+++ Assets/Scripts/Hurricane.cs
@@ -5,55 +5,49 @@ using UnityEngine;
 public class Hurricane : MonoBehaviour
 {
     [SerializeField] private int numSpheres = 100;
-    [SerializeField] private float outerRadius = 5f, innerRadius = 2f, minSpeed = 4f, maxSpeed = 10f;
+    [SerializeField] private float outerRadius = 5f, innerRadius = 2f, minSpeed = 4f, maxSpeed = 10f, height = 0f;
+    [SerializeField] private float sizeScalar = 1f, speedScalar = 1f;
     [SerializeField] GameObject spherePrefab;
 
     private GameObject[] spheres;
     private float[] angles, speeds, ellipseAngles;
-    private Vector2[] ellipticalAxes;
+    private Vector2[] baseEllipticalAxes;
 
     void Start()
     {
+        Application.targetFrameRate = 60;
         spheres = new GameObject[numSpheres];
         angles = new float[numSpheres];
         speeds = new float[numSpheres];
         ellipseAngles = new float[numSpheres];
-        ellipticalAxes = new Vector2[numSpheres];
-       
-
+        baseEllipticalAxes = new Vector2[numSpheres];
 
         for (int i = 0; i < numSpheres; i++)
         {
-
             spheres[i] = Instantiate(spherePrefab, Vector3.zero, Quaternion.identity);
             
             angles[i] = Random.Range(0f, 2f * Mathf.PI);
             ellipseAngles[i] = Random.Range(0f, 2f * Mathf.PI);
 
-
-            float a = ScewedRand(innerRadius, outerRadius); 
+            float a = ScewedRand(innerRadius, outerRadius);
             float b = ScewedRand(innerRadius, outerRadius);
 
-            ellipticalAxes[i] = new Vector2(a, b);
+            baseEllipticalAxes[i] = new Vector2(a, b);
 
-            float speedScalar = (a + b) / (outerRadius + outerRadius);
-            speeds[i] = BellCurveRand(minSpeed, maxSpeed, 2) * speedScalar;
+            float speedScalarFactor = (a + b) / (outerRadius + outerRadius);
+            speeds[i] = BellCurveRand(minSpeed, maxSpeed, 2) * speedScalarFactor;
         }
     }
 
     void Update()
     {
+        float h = height;
         for (int i = 0; i < numSpheres; i++)
         {
-
-            angles[i] += speeds[i] * Time.deltaTime;
-            spheres[i].transform.position = new Vector3(findXEllipse(i, angles[i]), 0, findZEllipse(i, angles[i]));
-
-            float nextAngle = angles[i] + speeds[i] * Time.deltaTime;
-            Vector3 nextPosition = new Vector3(findXEllipse(i, nextAngle), 0, findZEllipse(i, nextAngle));
-
+            angles[i] += speeds[i] * speedScalar * Time.deltaTime;
+            Vector2 scaledAxes = baseEllipticalAxes[i] * sizeScalar;
+            spheres[i].transform.position = new Vector3(findXEllipse(i, angles[i], scaledAxes), h, findZEllipse(i, angles[i], scaledAxes));
         }
-        
     }
 
     private float BellCurveRand(float min, float max, int times)
@@ -69,20 +63,19 @@ public class Hurricane : MonoBehaviour
     private float ScewedRand(float a, float b)
     {
         return (a - b) * Mathf.Sqrt(Random.Range(0f, 1f)) + b;
- 
     }
 
-    private float findXEllipse(int i, float t)
+    private float findXEllipse(int i, float t, Vector2 axes)
     {
-        //x = pcos(θ)cos(a)+qsin(θ)sin(a)
+        // x = pcos(θ)cos(a) + qsin(θ)sin(a)
         float a = ellipseAngles[i];
-        return ellipticalAxes[i].x * Mathf.Cos(t) * Mathf.Cos(a) + ellipticalAxes[i].y * Mathf.Sin(t) * Mathf.Sin(a);
+        return axes.x * Mathf.Cos(t) * Mathf.Cos(a) + axes.y * Mathf.Sin(t) * Mathf.Sin(a);
     }
 
-    private float findZEllipse(int i, float t)
+    private float findZEllipse(int i, float t, Vector2 axes)
     {
-        //z = −pcos(θ)sin(a)+qsin(θ)cos(a)
+        // z = −pcos(θ)sin(a) + qsin(θ)cos(a)
         float a = ellipseAngles[i];
-        return -1 * ellipticalAxes[i].x * Mathf.Cos(t) * Mathf.Sin(a) + ellipticalAxes[i].y * Mathf.Sin(t) * Mathf.Cos(a);
+        return -1 * axes.x * Mathf.Cos(t) * Mathf.Sin(a) + axes.y * Mathf.Sin(t) * Mathf.Cos(a);
     }
 }
